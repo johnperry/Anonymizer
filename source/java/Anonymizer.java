f729f108fd0f88220afb9d0f8518994012166a6e
John Perry <johnperry@dls.net>
2021-09-11 19:16:39 -0500

    Add in a smarter throttle

--- source/java/Anonymizer.java
+++ source/java/Anonymizer.java
@@ -32,7 +32,7 @@ import org.w3c.dom.Node;
  */
 public class Anonymizer extends JFrame {
 	
-    private String					windowTitle = "RSNA Anonymizer - version 10";
+    private String					windowTitle = "RSNA Anonymizer - version 11";
     private MainPanel				mainPanel;
     private JPanel					splitPanel;
     private WelcomePanel			welcomePanel;
